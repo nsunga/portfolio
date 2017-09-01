e12bcf57cbe8e2fc0ef47cb9c0640b155afd7007
Nick Sunga <nick.sunga95@gmail.com>
2017-08-31 17:59:08 -0700

    fixed the hovers. mobile is still UGLY

--- scripts/image_hover.js
+++ scripts/image_hover.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
   var madmaan_img = $("#madmaan_img");
-  var imagePos = madmaan_img.position();
+  var boxes_img = $("#boxes_img");
+  var gif_img = $("#gif_img");
+  var outfits_img = $("#outfits_img");
 
   $("#madmaan_txt").css("color", "white");
-  //$("#madmaan_txt").parent().css({position: 'relative'});
-  $("#madmaan_txt").css({top: imagePos.left, left: imagePos.top, position:'absolute'});
+  $("#madmaan_txt").css({top: madmaan_img.height()/2 - 20, left: madmaan_img.width()/2 - 20, position:'absolute'});
+  $("#boxes_txt").css("color", "white");
+  $("#boxes_txt").css({top: boxes_img.height()/2 - 20, left: boxes_img.width()/2 - 20, position:'absolute'});
+  $("#gif_txt").css("color", "white");
+  $("#gif_txt").css({top: gif_img.height()/2 - 20, left: gif_img.width()/2 - 20, position:'absolute'});
+  $("#outfits_txt").css("color", "white");
+  $("#outfits_txt").css({top: outfits_img.height()/2 - 20, left: outfits_img.width()/2 - 20, position:'absolute'});
+
   // GETTING SOMEWHERE. Look into the 'absolute' property
   var mPos = $("#madmaan_txt").position();
   console.log(mPos.left);
   console.log(mPos.top);
-  console.log(madmaan_img.position().top);
-  console.log(madmaan_img.position().left);
+  console.log('image height: ' + madmaan_img.height());
+  console.log('image width: ' + madmaan_img.width());
 
 });
